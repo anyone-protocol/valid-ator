c890dd60451e8c6e23d6fff585782431a457a6d7
kanshi <46557+kanshi@users.noreply.github.com>
2023-11-23 11:15:45 +0100

    Naive implementation of the Raft protocol

--- src/cluster/cluster.service.ts
+++ src/cluster/cluster.service.ts
@@ -1,6 +1,7 @@
 import { BeforeApplicationShutdown, Injectable, Logger, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Consul from 'consul'
+import { Append, AppendResult, Config, State, Vote, VoteResult } from './interfaces/raft-types';
 
 @Injectable()
 export class ClusterService implements OnApplicationBootstrap, BeforeApplicationShutdown {
@@ -60,4 +61,197 @@ export class ClusterService implements OnApplicationBootstrap, BeforeApplication
     async beforeApplicationShutdown(): Promise<void> {
         
     }
+
+    public requestVote(
+        configuration: Config,
+        rpc: Vote
+      ): VoteResult {
+        // If the RPC term is less than the current term, return a response with voteGranted set to false
+        if (rpc.term < configuration.currentTerm) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // If the RPC term is greater than the current term, update the current term and vote for the candidate
+        if (rpc.term > configuration.currentTerm) {
+          configuration.currentTerm = rpc.term;
+          configuration.votedFor = rpc.candidateId;
+        }
+      
+        // If the voter has already voted for a candidate in this term, return a response with voteGranted set to false
+        if (configuration.votedFor && configuration.votedFor !== rpc.candidateId) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // If the candidate's log is not up-to-date, return a response with voteGranted set to false
+        const lastLogIndex = configuration.log.length - 1;
+        const lastLogTerm = configuration.log[lastLogIndex].term;
+        if (lastLogTerm > rpc.lastLogTerm || (lastLogTerm === rpc.lastLogTerm && lastLogIndex > rpc.lastLogIndex)) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // Otherwise, return a response with voteGranted set to true
+        configuration.votedFor = rpc.candidateId;
+        return {
+          term: configuration.currentTerm,
+          voteGranted: true,
+        };
+      };
+
+    public appendEntries(
+        configuration: Config,
+        rpc: Append
+      ): AppendResult {
+        // If the RPC term is less than the current term, return a response with success set to false
+        if (rpc.term < configuration.currentTerm) {
+          return {
+            term: configuration.currentTerm,
+            success: false,
+            index: -1
+          };
+        }
+      
+        // If the RPC term is greater than the current term, update the current term and set the node's state to follower
+        if (rpc.term > configuration.currentTerm) {
+          configuration.currentTerm = rpc.term;
+          configuration.state = State.FOLLOWER;
+        }
+      
+        // If the previous log index and term don't match the node's log, return a response with success set to false
+        const prevLogIndex = rpc.prevLogIndex;
+        const prevLogTerm = rpc.prevLogTerm;
+        if (configuration.log[prevLogIndex]?.term !== prevLogTerm) {
+          return {
+            term: configuration.currentTerm,
+            success: false,
+            index: -1
+          };
+        }
+      
+        // Otherwise, append the new entries to the log and return a response with success set to true
+        configuration.log = [...configuration.log.slice(0, prevLogIndex + 1), ...rpc.entries];
+        configuration.commitIndex = Math.min(rpc.leaderCommit, configuration.log.length - 1);
+        return {
+          term: configuration.currentTerm,
+          success: true,
+          index: configuration.commitIndex
+        };
+      };
+
+    public startElection(configuration: Config): void {
+        // Increment the current term and set the node's state to candidate
+        configuration.currentTerm++;
+        configuration.state = State.CANDIDATE;
+        
+        // Reset the votedFor field
+        configuration.votedFor = undefined;
+        
+        // Request votes from other nodes
+        this.sendRequestVoteRPC(configuration);
+    };
+      
+    public sendRequestVoteRPC(configuration: Config): void {
+    // Implementation omitted for brevity
+    // Sends a RequestVoteRPC to other nodes in the cluster
+    };
+
+    // TODO: Connect to requests for rpc vote
+    public handleRequestVoteRPC(
+        configuration: Config,
+        rpc: Vote
+      ): VoteResult {
+        // If the RPC term is less than the current term, return a response with voteGranted set to false
+        if (rpc.term < configuration.currentTerm) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // If the RPC term is greater than the current term, update the current term and set the node's state to follower
+        if (rpc.term > configuration.currentTerm) {
+          configuration.currentTerm = rpc.term;
+          configuration.state = State.FOLLOWER;
+        }
+      
+        // If the node is already a leader or a candidate, return a response with voteGranted set to false
+        if (configuration.state === State.LEADER || configuration.state === State.CANDIDATE) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // If the node has already voted for another candidate in this term, return a response with voteGranted set to false
+        if (configuration.votedFor && configuration.votedFor !== rpc.candidateId) {
+          return {
+            term: configuration.currentTerm,
+            voteGranted: false,
+          };
+        }
+      
+        // Otherwise, return the result of the requestVote function
+        return this.requestVote(configuration, rpc);
+      };
+
+      // TODO: connect with appendEntriesRPC
+      public handleAppendEntriesRPC(
+        configuration: Config,
+        rpc: Append
+      ): AppendResult {
+        // If the RPC term is less than the current term, return a response with success set to false
+        if (rpc.term < configuration.currentTerm) {
+          return {
+            term: configuration.currentTerm,
+            success: false,
+            index: -1
+          };
+        }
+      
+        // If the RPC term is greater than the current term, update the current term and set the node's state to follower
+        if (rpc.term > configuration.currentTerm) {
+          configuration.currentTerm = rpc.term;
+          configuration.state = State.FOLLOWER;
+        }
+      
+        // If the node is a leader, return a response with success set to false
+        if (configuration.state === State.LEADER) {
+          return {
+            term: configuration.currentTerm,
+            success: false,
+            index: -1
+          };
+        }
+      
+        // Otherwise, return the result of the appendEntries function
+        return this.appendEntries(configuration, rpc);
+      };
+
+      public advanceCommitIndex(
+        configuration: Config,
+        responses: AppendResult[]
+      ): void {
+        // Sort the responses by term and index
+        responses.sort((a, b) => a.term !== b.term ? a.term - b.term : a.index - b.index);
+      
+        // Find the highest index that is included in a majority of responses
+        const majority = Math.floor(responses.length / 2) + 1;
+        let commitIndex = 0;
+        for (let i = 0; i < responses.length; i++) {
+          if (responses.slice(0, i + 1).filter((r) => r.success).length >= majority) {
+            commitIndex = responses[i].index;
+          }
+        }
+      
+        // Set the commit index to the highest index that is included in a majority of responses
+        configuration.commitIndex = commitIndex;
+      };
 }
---
+++ src/cluster/interfaces/raft-types.ts
@@ -0,0 +1,58 @@
+export type Term = number;
+export type LogIndex = number;
+
+export enum State {
+  FOLLOWER = "follower",
+  CANDIDATE = "candidate",
+  LEADER = "leader",
+}
+
+export type Id = string;
+
+export type Entry = {
+  term: Term;
+  command: any;
+};
+
+export type Log = Entry[];
+
+export type FSM = {
+  applyCommand: (command: any) => void;
+};
+
+export type Config = {
+  id: Id;
+  stateMachine: FSM;
+  currentTerm: Term;
+  votedFor?: Id;
+  log: Log;
+  state: State
+  commitIndex: LogIndex
+};
+
+export type Vote = {
+  term: Term;
+  candidateId: Id;
+  lastLogIndex: LogIndex;
+  lastLogTerm: Term;
+};
+
+export type VoteResult = {
+  term: Term;
+  voteGranted: boolean;
+};
+
+export type Append = {
+  term: Term;
+  leaderId: Id;
+  prevLogIndex: LogIndex;
+  prevLogTerm: Term;
+  entries: Entry[];
+  leaderCommit: LogIndex;
+};
+
+export type AppendResult = {
+  term: Term;
+  success: boolean;
+  index: LogIndex
+};
