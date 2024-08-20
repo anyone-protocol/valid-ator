958c1e1be9e3db7175c68da2c567da92d2c5d3a1
Jim Toth <jim@artby.city>
2024-08-20 15:48:50 -0400

    Re-add whitespace

--- src/verification/dto/verification-result-dto.ts
+++ src/verification/dto/verification-result-dto.ts
@@ -2,6 +2,7 @@ import { ValidatedRelay } from 'src/validation/schemas/validated-relay'
 import { RelayVerificationResult } from './relay-verification-result'
 
 export type VerificationResults = VerificationResultDto[]
+
 export class VerificationResultDto {
     readonly result: RelayVerificationResult
     readonly relay: ValidatedRelay
